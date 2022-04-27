2e6df971087d8d13a20ff5bb9d0005d4f241f053
Mostafa Alaa <mesteeve.alaa.hamdy@gmail.com>
2022-04-28 01:43:58 +0200

    Zuqa

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    // main main
+    // Zuqa
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
