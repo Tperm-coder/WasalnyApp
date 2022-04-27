f0f6fb6f884ecc2c9790b8d3b4085ff21e95f56d
Mostafa Alaa <mesteeve.alaa.hamdy@gmail.com>
2022-04-28 01:27:06 +0200

    5ello

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /media/hazem/projects/wasalny
+# Install script for directory: /home/beevo/Desktop/CLion Projects/Deliver me/walasnyApp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/media/hazem/projects/wasalny/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/beevo/Desktop/CLion Projects/Deliver me/walasnyApp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    // 5ello
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
